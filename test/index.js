22473cc8389dd5ecac7345f7e24b327a08e42511
Hasan HameedHameed <hasan.hameed@iomechs.com>
2020-10-26 15:35:02 +0500

    test

--- test/index.js
+++ test/index.js
@@ -9,33 +9,17 @@ const fs = require('fs');
 
 describe('rtl-generator test cases', function () {
     it('float test', async function () {
-        // const result = await rtlGenerator({
-        //     // inputFiles: [
-        //     //     // '/Users/mac/Projects/iraqi-souq-buyer/src/assets/scss/style.css',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/banner/banner.component.css',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/cart/components/cart.css',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/core/components/footer/footer.component.css',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/core/components/header/header.component.scss',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/order/components/invoice/invoice.component.css',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/order/components/order-box/order-box.component.css',
-        //     //     '/Users/mac/Projects/iraqi-souq-buyer/src/app/order/components/view/view.component.css',
-        //     // ]
-        //     folderPath: '/Users/mac/Projects/iraqi-souq-buyer/src/'
-        // });
-        // expect(result[0]).to.equal(file('./test/float-rtl.css'));
+        const result = await rtlGenerator({
+            returnOutputOnly: true,
+            inputFiles: ['./test/float.css']
+        });
+        expect(result[0]).to.equal(file('./test/float-rtl.css'));
     });
 
-    // it('margin test', async function () {
-    //     const result = await rtlGenerator({
-    //         returnOutputOnly: true,
-    //         inputFiles: ['./test/margin.css']
-    //     });
-    //     expect(result[0]).to.equal(file('./test/margin-rtl.css'));
-    // });
     it('margin test', async function () {
         const result = await rtlGenerator({
             returnOutputOnly: true,
-            inputFiles: ['./test/comments.css']
+            inputFiles: ['./test/margin.css']
         });
         expect(result[0]).to.equal(file('./test/margin-rtl.css'));
     });
